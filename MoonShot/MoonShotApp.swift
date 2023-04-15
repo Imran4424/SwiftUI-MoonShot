6b777aa1f310d4b01b370461ed28da607c6ea7c4
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-16 00:52:07 +0600

    project init

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Shah Md Imran Hossain on 16/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
