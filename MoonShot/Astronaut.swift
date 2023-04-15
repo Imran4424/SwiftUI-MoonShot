67a290bba1eea5f5e3d946a98c3eee3825988902
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-16 01:12:53 +0600

    Astronaut code

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Shah Md Imran Hossain on 16/4/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ MoonShot/BundleDecodeable.swift
@@ -0,0 +1,28 @@
+//
+//  BundleDecodeable.swift
+//  MoonShot
+//
+//  Created by Shah Md Imran Hossain on 16/4/23.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded 
+    }
+}
