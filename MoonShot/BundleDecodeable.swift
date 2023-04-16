34359592746a1ced8fb7cd0c99516596659e6334
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-16 12:42:34 +0600

    generic decodable for json decoding

--- MoonShot/BundleDecodeable.swift
+++ MoonShot/BundleDecodeable.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file: String) -> [String: Astronaut] {
+    func decode<GenericType: Codable>(_ file: String) -> GenericType {
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
@@ -19,7 +19,7 @@ extension Bundle {
         
         let decoder = JSONDecoder()
         
-        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+        guard let loaded = try? decoder.decode(GenericType.self, from: data) else {
             fatalError("Failed to decode \(file) from bundle.")
         }
         
