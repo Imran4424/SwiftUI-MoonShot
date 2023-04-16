c03c0be5db747bf355fe0da424230f606d09a8f6
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-16 15:10:17 +0600

    complete mission view

--- MoonShot/MissionView.swift
+++ MoonShot/MissionView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct MissionView: View {
+    let mission: Mission
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.6)
+                        .padding(.top)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Mission Highlights")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        
+                        Text(mission.description)
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.bottom)
+            }
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
     }
 }
 
 struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    
     static var previews: some View {
-        MissionView()
+        MissionView(mission: missions[0])
+            .preferredColorScheme(.dark)
     }
 }
